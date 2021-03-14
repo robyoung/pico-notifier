1c12f8d85a06a8d6e9ef4a6d80d4e133825afc41
Rob Young <rob@robyoung.digital>
2021-03-14 21:57:49 +0000

    Add first draft of pico side notifier
    - Can set state of LEDs.
    - Can assign key sequences to buttons.

---
+++ pico/code.py
@@ -0,0 +1,63 @@
+import board
+import busio
+import supervisor
+import time
+import usb_hid
+
+from adafruit_bus_device.i2c_device import I2CDevice
+import adafruit_dotstar
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+from digitalio import DigitalInOut, Direction, Pull
+
+import notifier
+
+# Pull CS pin low to enable level shifter
+cs = DigitalInOut(board.GP17)
+cs.direction = Direction.OUTPUT
+cs.value = 0
+
+# Set up APA102 pixels
+num_pixels = 16
+pixels = adafruit_dotstar.DotStar(board.GP18, board.GP19, num_pixels, brightness=0.1, auto_write=True)
+
+# Set up I2C for IO expander (addr: 0x20)
+i2c = busio.I2C(board.GP5, board.GP4)
+device = I2CDevice(i2c, 0x20)
+
+# Set up the keyboard
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+keysets = {}
+
+# Read button states from the I2C IO expander on the keypad
+def read_button_states(x, y):
+    pressed = [0] * 16
+    with device:
+        # Read from IO expander, 2 bytes (8 bits) correspond to the 16 buttons
+        device.write(bytes([0x0]))
+        result = bytearray(2)
+        device.readinto(result)
+        b = result[0] | result[1] << 8
+
+        # Loop through the buttons
+        for i in range(x, y):
+            if not (1 << i) & b:
+                pressed[i] = 1
+            else:
+                pressed[i] = 0
+    return pressed
+
+
+while True:
+    notifier.handle_command(supervisor.runtime, pixels, keysets, Keycode)
+
+    pressed = read_button_states(0, 16)
+    for button, keyset in keysets.items():
+        if pressed[button] == 1:
+            notifier.execute_keyset(keyset, kbd, layout, time)
+
+    time.sleep(0.1)
---
+++ pico/lib/notifier.py
@@ -0,0 +1,90 @@
+def set_led(args, pixels):
+    if len(args) < 4:
+        raise ValueError('not enoug args: {}'.format(args))
+
+    try:
+        button = int(args[0])
+    except ValueError as e:
+        raise ValueError('invalid button number: {}'.format(e))
+
+    try:
+        pixel_args = tuple([int(colour) for colour in args[1:4]])
+    except ValueError as e:
+        raise ValueError('invalid colour: {}'.format(e))
+
+    if len(args) > 4:
+        try:
+            pixel_args = pixel_args + (float(args[4]),)
+        except ValueError as e:
+            raise ValueError('invalid brightness: {}'.format(e))
+
+    pixels[button] = pixel_args
+
+
+def set_key(args, keysets, Keycode):
+    if len(args) < 2:
+        raise ValueError('not enough args: {}'.format(args))
+
+    try:
+        button = int(args[0])
+    except ValueError as e:
+        raise ValueError('invalid button number: {}'.format(e))
+
+    keyset = [parse_keycmd(arg, Keycode) for arg in args[1:]]
+
+    keysets[button] = tuple(keyset)
+
+
+def parse_keycmd(keycmd, Keycode):
+    if len(keycmd) < 2:
+        raise ValueError('key command not long enough: {}'.format(keycmd))
+
+    name = keycmd[0]
+    value = keycmd[1:]
+    # s = sleep, k = keys, w = write
+    if name == 's':
+        return name, float(value)
+    elif name == 'k':
+        try:
+            return name, [getattr(Keycode, part) for part in value.split('|')]
+        except AttributeError as e:
+            raise ValueError('invalid keycode {}'.format(e))
+    elif name == 'w':
+        return name, value
+    else:
+        raise ValueError('unknown key command {}'.format(keycmd))
+
+
+def execute_keyset(keyset, kbd, layout, time):
+    for (name, value) in keyset:
+        if name == 's':
+            time.sleep(value)
+        elif name == 'k':
+            kbd.send(*value)
+        elif name == 'w':
+            layout.write(value)
+
+def handle_command(runtime, pixels, keysets, Keycode):
+    if runtime.serial_bytes_available:
+        value = input()
+        parts = value.split(':', 1)
+
+        command = parts[0].upper()
+        if len(parts) > 1:
+            args = [arg.strip() for arg in parts[1].split(',')]
+        else:
+            args = []
+
+        try:
+            if command == 'IDENTIFY':
+                print('Notifier/0.1')
+                return
+            elif command == 'SET LED':
+                set_led(args, pixels)
+            elif command == 'SET KEY':
+                set_key(args, keysets, Keycode)
+            else:
+                raise ValueError('unkown command {}'.format(command))
+            print('OK')
+        except ValueError as e:
+            print('ERROR: {}'.format(e))
